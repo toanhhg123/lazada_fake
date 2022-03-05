894200c62cc709ba3a23796a1c271ca4fc1dc4db
toanhhg123 <toanhhg123@gmail.com>
2022-03-05 16:18:30 +0700

    firt commit

---
+++ js/main.js
@@ -0,0 +1,23 @@
+const header = document.querySelector('header');
+const bodyElement = document.querySelector('.body');
+const iconMenuListDown = document.querySelector('.header-body-foot-category i');
+
+const menuListDown = document.querySelector('.header-body-foot-category');
+
+
+
+menuListDown.onmouseover = ()=>{
+    iconMenuListDown.className = 'bi bi-chevron-up';
+}
+
+menuListDown.onmouseleave = ()=>{
+    iconMenuListDown.className = 'bi bi-chevron-down';
+
+}
+
+
+
+
+
+bodyElement.style.marginTop = `${header.offsetHeight + 20}px`
+console.log('heder',header.offsetHeight);
